efd1aa4a04f21c9ee21a4f82d6cca11100add6d8
Mir-web-creativ <126182714+Mir-web-creativ@users.noreply.github.com>
2023-05-03 15:43:51 +0300

    Mir

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Car {
+import java.sql.Driver;
+
+public class Car implements AutoCloseable {
+
+
+    public Car() {
+
+    }
+
+
+    public void close() {
+        System.out.println("Машина жабылып жатат.");
+    }
+
+
+    public void drive() {
+        System.out.println("Машина журуп жатат.");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+    try{
+        transport();
+
+    }catch (Exception e){
+        System.out.println(e.getMessage());
+    }
+    }
+         public static void transport() throws IOException{
+        try (Car car = new Car()) {
+            car.drive();
+        } finally {
+            System.out.println("Finish");
+
+        }
     }
 }
